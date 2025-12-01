3d1aa0c3ccdeb0b339b442bbbb881bf43b8547c6
Moye <moyeodiase@hotmail.co.uk>
2025-12-01 17:23:50 +0000

    Manage iris ssh config declaratively

--- systems/iris/configuration.nix
+++ systems/iris/configuration.nix
@@ -36,4 +36,42 @@
     git
   ];
 
+  programs.ssh.extraConfig = ''
+    Include /Users/moye/.colima/ssh_config
+
+    Host *
+        SetEnv TERM=alacritty
+        IdentityFile ~/.ssh/iris.pem
+
+    Match host pallas exec "ping -c1 -W3 10.0.100.204 >/dev/null 2>&1"
+        HostName 10.0.100.204
+        ServerAliveInterval 60
+
+    Host pallas
+        HostName 192.168.1.204
+
+    Match host hekate exec "ping -c1 -W3 10.0.100.110 >/dev/null 2>&1"
+        HostName 10.0.100.110
+        ServerAliveInterval 60
+
+    Host hekate
+        User admin
+        HostName 192.168.1.110
+        HostKeyAlias hekate
+
+    Match host herakles exec "ping -c1 -W3 10.0.100.97 >/dev/null 2>&1"
+        HostName 10.0.100.97
+        ServerAliveInterval 60
+
+    Host herakles
+        HostName 192.168.1.97
+        HostKeyAlias herakles
+
+    Host hermes
+        HostName 34.39.105.36
+
+    Host hestia
+        HostName hestia.local
+  '';
+
 }
