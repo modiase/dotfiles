3afea44fbc7edf08d7e5eac163da7c81562ab1bc
Moye <moyeodiase@hotmail.co.uk>
2025-12-01 17:22:13 +0000

    Update coder config

--- nix/nixpkgs/coder/default.nix
+++ nix/nixpkgs/coder/default.nix
@@ -10,6 +10,15 @@ let
     name = "config.yaml";
     text = builtins.readFile ./config/config.yaml;
   };
+
+  gooseHints = pkgs.writeTextFile {
+    name = ".goosehints";
+    text = builtins.readFile ./config/.goosehints;
+  };
+
+  goose-cli-patched = pkgs.goose-cli.overrideAttrs (oldAttrs: {
+    patches = (oldAttrs.patches or [ ]) ++ [ ./goose-prompt.patch ];
+  });
 in
 pkgs.writeShellScriptBin "coder" ''
   mkdir -p ~/.config/goose/custom_providers
@@ -22,10 +31,14 @@ pkgs.writeShellScriptBin "coder" ''
     ! cmp -s ${gooseConfig} ~/.config/goose/config.yaml && \
     cp ${gooseConfig} ~/.config/goose/config.yaml
 
+  [ ! -f ~/.config/goose/.goosehints ] || \
+    ! cmp -s ${gooseHints} ~/.config/goose/.goosehints && \
+    cp ${gooseHints} ~/.config/goose/.goosehints
+
   export GOOSE_DISABLE_KEYRING=1
   export GOOSE_PROVIDER="custom_herakles"
   export GOOSE_MODEL="cpatonn/Qwen3-Coder-30B-A3B-Instruct-AWQ-4bit"
   export CUSTOM_HERAKLES_API_KEY="dummy"
 
-  exec ${pkgs.goose-cli}/bin/goose "$@"
+  exec ${goose-cli-patched}/bin/goose "$@"
 ''
