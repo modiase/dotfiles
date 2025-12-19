61f8b1739711d5693aeb6a40c5f154f0097b1293
Moye Odiase <moyeodiase@gmail.com>
2025-12-19 09:49:32 +0000

    Improve gfx
    - Add help flag.
    - Auto-accept formatting changes by default.

--- fish/functions/gfx.fish
+++ fish/functions/gfx.fish
@@ -1,8 +1,27 @@
-argparse 'r/ref=' 'n/num=' -- $argv
+argparse h/help 'rev=' 'n/num=' no-auto-accept-changes -- $argv
 or return
 
-if test -n "$_flag_ref" -a -n "$_flag_num"
-    echo "Error: Cannot use both -r/--ref and -n/--num" >&2
+if test -n "$_flag_help"
+    echo "Usage: gfx [OPTIONS]"
+    echo ""
+    echo "Git fixup - interactively select a commit to fixup and autosquash rebase"
+    echo ""
+    echo "Options:"
+    echo "  -h, --help                  Show this help message"
+    echo "  --rev=REVISION              Base revision for finding commits (default: main)"
+    echo "  -n, --num=NUMBER            Number of commits back from HEAD to use as base"
+    echo "  --no-auto-accept-changes    Don't automatically amend hook-modified files during rebase"
+    echo ""
+    echo "Examples:"
+    echo "  gfx                         # Fixup commits since main"
+    echo "  gfx --rev=develop           # Fixup commits since develop branch"
+    echo "  gfx -n 5                    # Fixup commits in last 5 commits"
+    echo "  gfx --no-auto-accept-changes # Manual review of hook changes"
+    return 0
+end
+
+if test -n "$_flag_rev" -a -n "$_flag_num"
+    echo "Error: Cannot use both --rev and -n/--num" >&2
     return 1
 end
 
@@ -17,17 +36,17 @@ else
     return 1
 end
 
-if test -z "$_flag_ref" -a -z "$_flag_num"
-    set _flag_ref main
+if test -z "$_flag_rev" -a -z "$_flag_num"
+    set _flag_rev main
 end
 
 set -l merge_base
 if test -n "$_flag_num"
     set merge_base HEAD~$_flag_num
 else
-    set merge_base (git merge-base HEAD $_flag_ref)
+    set merge_base (git merge-base HEAD $_flag_rev)
     if test $status -ne 0
-        echo "Failed to find merge base with $_flag_ref" >&2
+        echo "Failed to find merge base with $_flag_rev" >&2
         return 1
     end
 end
@@ -53,4 +72,9 @@ if test $status -ne 0
     return 1
 end
 
-git rebase --autosquash $merge_base
+if test -z "$_flag_no_auto_accept_changes"
+    # Hooks may modify files during rebase; auto-amend changes to prevent rebase halt
+    git rebase --autosquash $merge_base --exec 'git diff --quiet || (git add -u && git commit --amend --no-edit --no-verify)'
+else
+    git rebase --autosquash $merge_base
+end
