eff3d145f4c14290c7ce0084632d31be547eca9c
Moye Odiase <moyeodiase@gmail.com>
2026-01-08 14:07:12 +0000

    Clean up
    - Remove plugin defined functions from repo.
    - Remove fisher.
    - Use nix to manage fish plugins.
    - Export moor flags globally in fish.
    - Remove unused ren command.

---
+++ fish/functions/fzf_fish_help.fish
@@ -0,0 +1,6 @@
+nix shell nixpkgs#gum -c gum style --border rounded --padding "0 1" --border-foreground 6 \
+    "Ctrl+T  directory search" \
+    "Ctrl+G  git status" \
+    "Ctrl+R  history" \
+    "Ctrl+O  variables" \
+    "Ctrl+S  change directory"
