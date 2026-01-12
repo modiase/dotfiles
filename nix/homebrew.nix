4864d805778c8e1ad3064e571d4985f2cf185a1f
Moye Odiase <moyeodiase@gmail.com>
2026-01-12 11:16:44 +0000

    Temporarily disable hombrew

--- nix/homebrew.nix
+++ nix/homebrew.nix
@@ -1,6 +1,7 @@
 {
   homebrew = {
-    enable = true;
+    # Homebrew doesn't support macOS 26.2 yet
+    enable = false;
     onActivation.cleanup = "none";
     casks = [
       "font-hack-nerd-font"
