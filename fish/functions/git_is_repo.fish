fc15757a6104712fc46f09ae7d1655ed0be5bd8d
Moye Odiase <moyeodiase@gmail.com>
2026-01-26 18:26:39 +0000

    Simplify git-is-repo
    Fix edge case where .git exists but is not a valid git repo. Use
    rev-parse --git-dir which is more robust and simpler.

--- fish/functions/git_is_repo.fish
+++ fish/functions/git_is_repo.fish
@@ -1,8 +1,3 @@
 function git_is_repo -d "Check if directory is a repository"
-    test -d .git
-    or begin
-        set -l info (command git rev-parse --git-dir --is-bare-repository 2>/dev/null)
-        and test $info[2] = false
-        and test (command git rev-parse --show-toplevel 2>/dev/null) = (pwd)
-    end
+    command git rev-parse --git-dir >/dev/null 2>&1
 end
