08d53079d9ea2d42da720b750c899020fde3db7f
Moye <moyeodiase@hotmail.co.uk>
2026-02-01 09:34:40 +0000

    Remove broken config

--- nix/nixpkgs/ankigen/default.nix
+++ nix/nixpkgs/ankigen/default.nix
@@ -16,14 +16,6 @@ pkgs.buildGoModule {
 
   vendorHash = "sha256-msaYLbOFOK3xGDWK0t2+HnwH+8XHOKS4RIbgtDYA2tE=";
 
-  nativeBuildInputs = [ pkgs.makeBinaryWrapper ];
-
-  postInstall = ''
-    wrapProgram $out/bin/ankigen \
-      --argv0 ankigen \
-      --prefix PATH : ${lib.makeBinPath [ secrets ]}
-  '';
-
   meta = with lib; {
     description = "Generate Anki flashcards using AI with web search";
     mainProgram = "ankigen";
