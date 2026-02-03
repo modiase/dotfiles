a2c7bf8b2770abe03b28d037b2d8d4264f4f3b41
Moye Odiase <moyeodiase@gmail.com>
2026-02-03 11:15:08 +0000

    Add spinner for slow telescope queries

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -34,9 +34,75 @@ return {
 		})
 		require("telescope").load_extension("fzf")
 
+		local function call_with_spinner(picker_fn, opts)
+			opts = opts or {}
+
+			local ok, fidget = pcall(require, "fidget.progress.handle")
+			if not ok then
+				picker_fn(opts)
+				return
+			end
+
+			local fidget_handle = nil
+			local poll_timer = nil
+
+			local function stop_spinner()
+				if poll_timer then
+					poll_timer:stop()
+					poll_timer = nil
+				end
+				if fidget_handle then
+					fidget_handle:finish()
+					fidget_handle = nil
+				end
+			end
+
+			local function poll_for_results()
+				local bufnr = vim.api.nvim_get_current_buf()
+				if vim.bo[bufnr].filetype ~= "TelescopePrompt" then
+					stop_spinner()
+					return
+				end
+
+				local picker = require("telescope.actions.state").get_current_picker(bufnr)
+				if picker and picker.manager and picker.manager:num_results() > 0 then
+					stop_spinner()
+				end
+			end
+
+			local function start_spinner()
+				if fidget_handle then
+					return
+				end
+
+				fidget_handle = fidget.create({
+					title = "Codesearch",
+					message = "Searching...",
+					lsp_client = { name = "codesearch" },
+				})
+
+				poll_timer = vim.uv.new_timer()
+				poll_timer:start(200, 100, vim.schedule_wrap(poll_for_results))
+			end
+
+			local original_input_filter = opts.on_input_filter_cb
+			opts.on_input_filter_cb = function(prompt)
+				if #prompt > 0 then
+					start_spinner()
+				end
+
+				if original_input_filter then
+					return original_input_filter(prompt)
+				end
+				return { prompt = prompt }
+			end
+
+			picker_fn(opts)
+		end
+
 		vim.keymap.set("n", "<leader>fg", function()
 			if is_citc() then
-				require("telescope").extensions.codesearch.find_query()
+				call_with_spinner(require("telescope").extensions.codesearch.find_query)
 			else
 				require("telescope").extensions.live_grep_args.live_grep_args()
 			end
@@ -53,7 +119,7 @@ return {
 
 		vim.keymap.set("n", "<leader>ff", function()
 			if is_citc() then
-				require("telescope").extensions.codesearch.find_files()
+				call_with_spinner(require("telescope").extensions.codesearch.find_files)
 			else
 				require("telescope.builtin").find_files({ hidden = true })
 			end
