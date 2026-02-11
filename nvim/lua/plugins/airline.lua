fadb0b6f1811285aeae212eb97b85fec15183b71
Moye Odiase <moyeodiase@gmail.com>
2026-02-11 19:41:12 +0000

    Update airline colors
    Previous highlighitng was hiding active buffers in inactive tabs.

--- nvim/lua/plugins/airline.lua
+++ nvim/lua/plugins/airline.lua
@@ -11,19 +11,6 @@ return {
 	end,
 	config = function()
 		local colors = require("colors")
-		vim.api.nvim_create_autocmd("VimEnter", {
-			callback = function()
-				local palette = vim.g["airline#themes#base16#palette"]
-				if palette then
-					palette.tabline = {
-						airline_tab = { colors.base04, colors.base01, 245, 235, "" },
-						airline_tabsel = { colors.foreground, colors.base02, 254, 237, "bold" },
-						airline_tabfill = { colors.base03, colors.base00, 243, 234, "" },
-					}
-					vim.g["airline#themes#base16#palette"] = palette
-					vim.cmd("AirlineRefresh")
-				end
-			end,
-		})
+		vim.api.nvim_set_hl(0, "airline_tab", { fg = colors.base04, bg = colors.base01 })
 	end,
 }
