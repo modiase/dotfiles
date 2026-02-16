5dd658102073e6ad4bfec98da04d5aca67bd7869
Moye Odiase <moyeodiase@gmail.com>
2026-02-16 11:00:35 +0000

    Open yazi files in leftmost, non-terminal buffer

--- nix/nixpkgs/yazi/yazi-nvim.sh
+++ nix/nixpkgs/yazi/yazi-nvim.sh
@@ -3,6 +3,9 @@
 cmd="$1"
 shift
 
+# Vimscript to find first non-terminal window, or create left-split if none exist
+SELECT_EDIT_WIN=':let _f=0 | for w in range(1,winnr("$")) | if getbufvar(winbufnr(w),"&buftype")!=#"terminal" | exe w."wincmd w" | let _f=1 | break | endif | endfor | if !_f | aboveleft vnew | endif<CR>'
+
 get_nvim_socket() {
     [ -z "$TMUX" ] && return 1
     TARGET_PANE=$(tmux list-panes -F '#{pane_id} #{pane_current_command}' | grep -i nvim | head -n 1 | cut -d' ' -f1)
@@ -16,20 +19,20 @@ case "$cmd" in
     open)
         get_nvim_socket || exec nvim "$@"
         if [ -d "$1" ]; then
-            nvr --remote-send "<Esc>:cd $1<CR>"
+            nvr --remote-send "<C-\\><C-n>:cd $1<CR>"
         else
-            nvr --remote-send "<Esc>:silent tabedit $1<CR>"
+            nvr --remote-send "<C-\\><C-n>${SELECT_EDIT_WIN}:silent edit $1<CR>"
         fi
         tmux select-pane -t "$TARGET_PANE"
         ;;
     split)
         get_nvim_socket || exec nvim "$@"
-        nvr --remote-send "<Esc>:silent sp $1<CR>"
+        nvr --remote-send "<C-\\><C-n>${SELECT_EDIT_WIN}:silent sp $1<CR>"
         tmux select-pane -t "$TARGET_PANE"
         ;;
     vsplit)
         get_nvim_socket || exec nvim "$@"
-        nvr --remote-send "<Esc>:silent vs $1<CR>"
+        nvr --remote-send "<C-\\><C-n>${SELECT_EDIT_WIN}:silent vs $1<CR>"
         tmux select-pane -t "$TARGET_PANE"
         ;;
     cd)
@@ -39,7 +42,7 @@ case "$cmd" in
         [ -f "$path" ] && path="$(dirname "$path")"
         [ ! -d "$path" ] && echo "Error: not a directory: $path" >&2 && exit 1
         get_nvim_socket || exit 0
-        nvr --remote-send "<Esc>:cd $path<CR>"
+        nvr --remote-send "<C-\\><C-n>:cd $path<CR>"
         ;;
     *)
         echo "Usage: yazi-nvim {open|cd|split|vsplit} [args...]" >&2
