1250022330d805bc191bc8b6ba5abef304f5b0c1
Moye Odiase <moyeodiase@gmail.com>
2026-02-17 21:08:32 +0000

    Support secrets from stdin
    - Run tests for secrets during check phase.
    - Allow reading of secrets from stdin during store command.

--- nix/nixpkgs/secrets/default.nix
+++ nix/nixpkgs/secrets/default.nix
@@ -5,46 +5,53 @@
   lib,
   stdenv,
   pass,
-  makeWrapper,
+  makeBinaryWrapper,
 }:
 
 let
-  python = python313.withPackages (ps: [
+  pythonDeps = ps: [
     ps.click
     ps.cryptography
     ps.google-cloud-secret-manager
     ps.loguru
     ps.rich
-  ]);
-
-  schemas = stdenv.mkDerivation {
-    name = "secrets-schemas";
-    src = ./schemas;
-    installPhase = ''
-      mkdir -p $out/share/secrets/schemas
-      cp *.json $out/share/secrets/schemas/
-    '';
-  };
-
+  ];
+  python = python313.withPackages pythonDeps;
+  testPython = python313.withPackages (ps: pythonDeps ps ++ [ ps.pytest ]);
   runtimeDeps = [ google-cloud-sdk ] ++ lib.optionals stdenv.isLinux [ pass ];
 in
 stdenv.mkDerivation {
   pname = "secrets";
   version = "2.0.0";
-  src = ./secrets;
+  src = ./.;
 
-  nativeBuildInputs = [ makeWrapper ];
+  nativeBuildInputs = [ makeBinaryWrapper ];
   buildInputs = [ python ];
-  propagatedBuildInputs = [
-    google-cloud-sdk
-  ]
-  ++ lib.optionals stdenv.isLinux [ gnupg ];
+  nativeCheckInputs = [ testPython ];
+  propagatedBuildInputs = [ google-cloud-sdk ] ++ lib.optionals stdenv.isLinux [ gnupg ];
+
+  buildPhase = ''
+    mkdir -p bin
+    cat > bin/secrets << EOF
+    #!${testPython}/bin/python
+    import sys
+    sys.path.insert(0, "$PWD")
+    from secrets.cli import main
+    main()
+    EOF
+    chmod +x bin/secrets
+  '';
+
+  doCheck = true;
+  checkPhase = ''
+    PATH="$PWD/bin:$PATH" ${testPython}/bin/pytest test_unit.py test_integration.py -v
+  '';
 
   installPhase = ''
     mkdir -p $out/lib/secrets $out/bin $out/share/secrets/schemas
 
-    cp -r . $out/lib/secrets/
-    cp ${schemas}/share/secrets/schemas/*.json $out/share/secrets/schemas/
+    cp -r secrets/* $out/lib/secrets/
+    cp schemas/*.json $out/share/secrets/schemas/
 
     cat > $out/bin/.secrets-unwrapped << EOF
     #!${python}/bin/python
@@ -55,7 +62,7 @@ stdenv.mkDerivation {
     EOF
     chmod +x $out/bin/.secrets-unwrapped
 
-    makeWrapper $out/bin/.secrets-unwrapped $out/bin/secrets \
+    makeBinaryWrapper $out/bin/.secrets-unwrapped $out/bin/secrets \
       --prefix PATH : ${lib.makeBinPath runtimeDeps}
   '';
 
