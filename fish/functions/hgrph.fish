a8b3a5b4a16ec9f7501697bf59a8fda34dd0ba44
Moye Odiase <moyeodiase@gmail.com>
2026-04-04 23:00:05 +0100

    hgrph: show only merged commits by default

--- fish/functions/hgrph.fish
+++ fish/functions/hgrph.fish
@@ -3,5 +3,5 @@ if _is_hg
 else
     viddy -n 2 -t -w -x git log --graph --topo-order --color=always \
         '--format=%C(bold yellow)%h%C(reset) %s%C(auto)%d %C(dim)(%cr)%C(reset)' \
-        HEAD --all $argv
+        HEAD $argv
 end
