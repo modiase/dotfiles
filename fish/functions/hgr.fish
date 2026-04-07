36b13e29ba051186b9d33e5ecf10a835b6330397
Moye Odiase <moyeodiase@gmail.com>
2026-04-07 15:57:11 +0100

    Pass --color always to mercurial graph command

--- fish/functions/hgr.fish
+++ fish/functions/hgr.fish
@@ -1,5 +1,5 @@
 if _is_hg
-    viddy -n 2 -t -w hg xl $argv
+    viddy -n 2 -t -w hg --color always xl $argv
 else
     viddy -n 2 -t -w -x git log --graph --topo-order --color=always \
         '--format=%C(bold yellow)%h%C(reset) %s%C(auto)%d %C(dim)(%cr)%C(reset)' \
