f24f6fd0f4e9ee36f9331185769bee9dbebaa5fe
Moye Odiase <moyeodiase@gmail.com>
2026-04-08 13:31:39 +0100

    telescope: replace find_files with frecency for recency-ranked file picking

---
+++ nvim/lua/plugins/frecency.lua
@@ -0,0 +1,16 @@
+return {
+	"nvim-telescope/telescope-frecency.nvim",
+	dependencies = { "nvim-telescope/telescope.nvim" },
+	event = "VeryLazy",
+	config = function()
+		require("telescope").setup({
+			extensions = {
+				frecency = {
+					default_workspace = "CWD",
+					show_filter_column = false,
+				},
+			},
+		})
+		require("telescope").load_extension("frecency")
+	end,
+}
