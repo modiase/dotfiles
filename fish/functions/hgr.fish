6e4ba11ecba09a21cfeb05c4e2a139705f4ef723
Moye Odiase <moyeodiase@gmail.com>
2026-04-08 07:58:18 +0100

    hgr: log short time

--- fish/functions/hgr.fish
+++ fish/functions/hgr.fish
@@ -1,7 +1,14 @@
 if _is_hg
     viddy -n 2 -t -w hg --color always xl $argv
 else
-    viddy -n 2 -t -w -x git log --graph --topo-order --color=always \
-        '--format=%C(bold yellow)%h%C(reset) %s%C(auto)%d %C(dim)(%cr)%C(reset)' \
-        HEAD $argv
+    viddy -n 2 -t -w -x bash -c 'git log --graph --topo-order --color=always \
+        --format="%C(bold yellow)%h%C(reset) %s%C(auto)%d %C(dim)(%cr)%C(reset)" \
+        HEAD "$@" | sed -E \
+        -e "s/([0-9]+) seconds? ago/\1s/g" \
+        -e "s/([0-9]+) minutes? ago/\1m/g" \
+        -e "s/([0-9]+) hours? ago/\1h/g" \
+        -e "s/([0-9]+) days? ago/\1d/g" \
+        -e "s/([0-9]+) weeks? ago/\1w/g" \
+        -e "s/([0-9]+) months? ago/\1mo/g" \
+        -e "s/([0-9]+) years? ago/\1y/g"' -- $argv
 end
