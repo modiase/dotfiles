4e734246915a8aea0d07a64b98ba51cbe35a2ca5
Moye Odiase <moyeodiase@gmail.com>
2026-04-08 07:32:01 +0100

    devlogs: reduce msg level

--- nvim/lua/views/noice-devlogs.lua
+++ nvim/lua/views/noice-devlogs.lua
@@ -4,17 +4,15 @@ local log = require("devlogs").new("nvim-notify")
 local notify_level_map = {
 	error = log.error,
 	warn = log.warning,
-	info = log.info,
-	debug = log.debug,
-	trace = log.debug,
 }
 
 local msg_show_kind_map = {
-	emsg = log.error,
-	echoerr = log.error,
 	lua_error = log.error,
 	rpc_error = log.error,
-	wmsg = log.warning,
+}
+
+local promotions = {
+	{ pattern = "written$", fn = log.info },
 }
 
 ---@class DevlogsView: NoiceView
@@ -24,8 +22,20 @@ local DevlogsView = View:extend("DevlogsView")
 
 function DevlogsView:show()
 	for _, m in ipairs(self._messages) do
-		local fn = notify_level_map[m.level] or msg_show_kind_map[m.kind] or log.info
-		fn(m:content())
+		local content = m:content()
+		local fn = m.level and notify_level_map[m.level]
+		if not fn and m.kind and m.kind ~= "" then
+			fn = msg_show_kind_map[m.kind]
+		end
+		if not fn then
+			for _, p in ipairs(promotions) do
+				if content:find(p.pattern) then
+					fn = p.fn
+					break
+				end
+			end
+		end
+		(fn or log.debug)(content)
 	end
 	self:clear()
 end
