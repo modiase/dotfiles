8ed9dbab9932f5a5d0bf1aec1594dac4b488f999
Moye Odiase <moyeodiase@gmail.com>
2026-04-27 14:53:19 +0100

    gst,glg: add -w/--watch to live-tail via viddy

--- fish/functions/glg.fish
+++ fish/functions/glg.fish
@@ -1,5 +1,22 @@
+set -l watch 0
+set -l rest
+for arg in $argv
+    switch $arg
+        case -w --watch
+            set watch 1
+        case '*'
+            set -a rest $arg
+    end
+end
+
 if _is_hg
-    hg xl $argv
+    if test $watch -eq 1
+        viddy -t -w hg --color always xl $rest
+    else
+        hg xl $rest
+    end
+else if test $watch -eq 1
+    viddy -t -w git -c color.ui=always log --oneline $rest
 else
-    git log --oneline $argv
+    git log --oneline $rest
 end
--- fish/functions/gst.fish
+++ fish/functions/gst.fish
@@ -1,5 +1,22 @@
+set -l watch 0
+set -l rest
+for arg in $argv
+    switch $arg
+        case -w --watch
+            set watch 1
+        case '*'
+            set -a rest $arg
+    end
+end
+
 if _is_hg
-    hg status $argv
+    if test $watch -eq 1
+        viddy -t -w hg --color always status $rest
+    else
+        hg status $rest
+    end
+else if test $watch -eq 1
+    viddy -t -w git -c status.color=always status --short $rest
 else
-    git status $argv
+    git status $rest
 end
